4c750a0a664256bdc775c584b21d21fd872c274c
ujiro99 <siro.cola@gmail.com>
2024-03-28 21:38:50 +0900

    update: 古いコンフィグファイルが存在したらそちらを読み込む

--- config.go
+++ config.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"os"
 	"path"
@@ -10,14 +11,39 @@ import (
 
 var configFile = "config.json"
 var packageName = "browser-profile-launcher"
+var packageNameOld = "chrome-profile-selector"
 
 func Load() (string, error) {
 	configFilePath, err := xdg.ConfigFile(path.Join(packageName, configFile))
 	if err != nil {
 		log.Println("コンフィグファイルが見つかりません:", configFilePath)
 	}
+	ret, err := loadFile(configFilePath)
+	if err == nil {
+		return ret, nil
+	}
 
-	text, err := os.ReadFile(configFilePath)
+	oldFilePath, err := xdg.ConfigFile(path.Join(packageNameOld, configFile))
+	if err != nil {
+		log.Println("コンフィグファイルが見つかりません:", oldFilePath)
+	}
+	ret, err = loadFile(oldFilePath)
+	if err == nil {
+		return ret, nil
+	}
+
+	log.Println("コンフィグファイルの読み込み失敗")
+	return "", errors.New("Failed to load config file")
+}
+
+func loadFile(filePath string) (string, error) {
+	_, err := os.Stat(filePath)
+	if os.IsNotExist(err) {
+		log.Println("コンフィグファイルが存在しません:", err)
+		return "", err
+	}
+
+	text, err := os.ReadFile(filePath)
 	if err != nil {
 		log.Println("コンフィグファイルを読み込めません:", err)
 		return "", err
