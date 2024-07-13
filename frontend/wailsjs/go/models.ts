75528cf3aeb68ac1b11889c623b5a96c1ba06fac
ujiro99 <siro.cola@gmail.com>
2024-07-13 14:35:48 +0900

    update: ビルド時の変更点をコミット

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -1,20 +1,3 @@
-export namespace options {
-	
-	export class SecondInstanceData {
-	
-	
-	    static createFrom(source: any = {}) {
-	        return new SecondInstanceData(source);
-	    }
-	
-	    constructor(source: any = {}) {
-	        if ('string' === typeof source) source = JSON.parse(source);
-	
-	    }
-	}
-
-}
-
 export namespace profile {
 	
 	export class Profile {
--- go.mod
+++ go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/adrg/xdg v0.4.0
 	github.com/fsnotify/fsnotify v1.7.0
 	github.com/tidwall/gjson v1.9.3
-	github.com/wailsapp/wails/v2 v2.9.1
+	github.com/wailsapp/wails/v2 v2.8.0
 	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1
 )
 
